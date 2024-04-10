53ec55f1fd8b00ba92789208ef05c8c92b60b691
mercelovidal <marcelo.vidal2023@alu.uct.cl>
2024-04-10 18:08:18 -0400

    NECESARIO PARA EL CODIGO
    Aqui esta el codigo .py y las imagenes que se usaran para los codigos.

---
+++ Proyecto_01.py
@@ -0,0 +1,74 @@
+#---------------------------------------------
+#       Importaciones
+#---------------------------------------------
+import pygame as pg, random as ra
+import ctypes as ct
+from pygame.locals import *
+
+n_res = (480,620); pxWidht = pxHeigth = 32; n_robots = 1
+
+
+#--------------------------------------------
+#       Estructura.
+#--------------------------------------------
+class eRobot(ct.Structure):
+    _fields_ = [
+        #-- completaaaaaar...
+    ]
+
+#-------------------------------------------
+#       Carga de Archivos.
+#-------------------------------------------
+def Load_Image():
+    #-- completaaaaaar...
+    pass
+
+#-------------------------------------------
+#       Init Pygame
+#-------------------------------------------
+def init_Pygame():
+    #-- completaaaaaar...
+    pass
+
+#-------------------------------------------
+#       Init robot.
+#-------------------------------------------
+def init_Robot():
+    #-- completaaaaaar...
+    pass
+
+#-------------------------------------------
+#       SE PINTA EL ROBOT EN LA SUPERFICIE
+#-------------------------------------------
+def Show_Mouse():
+    #-- completaaaaaar...
+    # sWin.blit(ddt['A'],(nMx,nMy))
+    pass
+
+#--------------------------------------------
+#       MUEVE AL ROBOT
+#--------------------------------------------
+def Move_Robot():
+    #-- completaaaaaar...
+    pass
+
+#---------------------------------------------
+#       Init de los Tiles(imagenes)
+#---------------------------------------------
+def Init_Tiles():
+    #-- completaaaaaar...
+    pass
+
+#----------------------------------------------
+#       MUESTRA EL MAPA
+#---------------------------------------------
+def Show_Map():
+    pass
+
+clock = pg.time.Clock()
+Flag = True
+while Flag:
+    #-- completaaaaaar...
+    break
+
+pg.quit
