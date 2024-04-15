40304353c5f8547875bc15c52b6614a29f2a97c1
mercelovidal <marcelo.vidal2023@alu.uct.cl>
2024-04-15 18:02:45 -0400

    codigo 1 mas completo

--- Proyecto_01.py
+++ Proyecto_01.py
@@ -2,119 +2,114 @@
 #       Importaciones 
 #---------------------------------------------
 import pygame as pg, random as ra
+import ctypes as ct
 from pygame.locals import *
 
-n_RES = (512,640); pxWIDTH = pxHEIGTH = 32;
-nMOUSE_x = nMOUSE_y = 0; nMAX_COL = n_RES[0] /  pxWIDTH
-nMAX_FIL = n_RES[1] / pxHEIGTH; bGo= True
-
-#-------------------------------------------
-#       Carga de Archivos.
-#-------------------------------------------
+n_RES = (512,640); pxWIDTH = pxHEIGTH = 32; 
+n_MAXFIL = n_RES[1] / pxHEIGTH; n_MAXCOL = n_RES[0] / pxWIDTH;
+n_MOUSEy = n_MOUSEx = 0; fGO = True
+#--------------------------------------------------------            
+# Carga imagenes y convierte formato PyGame
+#--------------------------------------------------------
 def Load_Image(sFile,transp = False):
     try: image = pg.image.load(sFile)
     except pg.error,message:
-        raise SystemExit,message
+           raise SystemExit,message
     image = image.convert()
     if transp:
        color = image.get_at((0,0))
        image.set_colorkey(color,RLEACCEL)
     return image
-    #-- completaaaaaar...
 
 
 #-------------------------------------------
 #       Init Pygame
 #-------------------------------------------
-def init_Pygame():
+def Init_Pygame():
     pg.init()
-    pg.mouse.set_visible(False)
-    pg.display.set_caption('Proyecto1.1 - Programacion de Robot')
+    pg.mouse.set_visible(False) 
+    pg.display.set_caption(' Mapas 2D Robotica - By ACS')
     return pg.display.set_mode(n_RES)
 
 #-------------------------------------------
-#       Init robot.
-#-------------------------------------------
-def mod_Mapa():
-    pass
-
-#-------------------------------------------
-#       SE PINTA EL MOUSE EN LA SUPERFICIE
+#       SE PINTA EL MOUSE LA SUPERFICIE
 #-------------------------------------------
 def Show_Mouse():
-    sWin.blit(ddt['9'],(nMOUSE_x,nMOUSE_y))
-    return
+    sWin.blit(ddt['9'],(n_MOUSEx,n_MOUSEy))
+    return 
 
 #---------------------------------------------
 #       Init de los Tiles(imagenes)
 #---------------------------------------------
 def Init_Tiles():
-    a_img = []
-    a_img.append(Load_Image('T00.png')) # Tile Tierra 1
-    a_img.append(Load_Image('T01.png')) # Tile Tierra 2
-    a_img.append(Load_Image('T02.png')) # Tile Piedra
-    a_img.append(Load_Image('T03.png')) # Tile Star Azul
-    a_img.append(Load_Image('T04.png')) # Tile Star Roja
-    a_img.append(Load_Image('T05.png')) # Tile Star Yellow
-    a_img.append(Load_Image('T06.png')) # Tile Gris Claro
-    a_img.append(Load_Image('T07.png')) # Tile Mostaza
-    a_img.append(Load_Image('T08.png')) # Tile Celeste
-    a_img.append(Load_Image('T09.png',True)) # Mouse
-    return a_img
-
+    aImg = [] # Array de Tiles y Sprites
+    aImg.append(Load_Image('T00.png')) # Tile Tierra 1
+    aImg.append(Load_Image('T01.png')) # Tile Tierra 2
+    aImg.append(Load_Image('T02.png')) # Tile Piedra
+    aImg.append(Load_Image('T03.png')) # Tile Star Azul
+    aImg.append(Load_Image('T04.png')) # Tile Star Roja
+    aImg.append(Load_Image('T05.png')) # Tile Star Yellow
+    aImg.append(Load_Image('T06.png')) # Tile Gris Claro
+    aImg.append(Load_Image('T07.png')) # Tile Mostaza
+    aImg.append(Load_Image('T08.png')) # Tile Celeste
+    aImg.append(Load_Image('T10.png',True)) # Mouse
+    return aImg
 
 #----------------------------------------------
 #       MUESTRA EL MAPA
 #---------------------------------------------
 def Show_Map():
-    #revisar tabulaciones
-    n_colu = n_fila = 0
-    for nF in range(0,n_RES[1] / pxHEIGTH):           #Itera en las Filas
-        for nC in range(0, n_RES[0] / pxWIDTH):       #Itera en las Columnas
-            sWin.blit(matriz_Map[nF][nC], (n_colu, n_fila))
-            n_colu += pxWIDTH
-        n_colu= 0
-        n_fila += pxHEIGTH
-    return
-#----------------------------------------------
-#       WHILE PRINCIPAL
-#----------------------------------------------
-sWin = init_Pygame()            
+    nCol = 0 ; nFil = 0
+    for nF in range(0,n_RES[1] / pxHEIGTH): # Iter. x Filas
+        for nC in range(0,n_RES[0] / pxWIDTH): # Iter. x Colum
+            sWin.blit(aMapa[nF][nC],(nCol,nFil))
+            nCol += pxWIDTH 
+        nCol = 0; nFil += pxHEIGTH 
+#-------------------------------------------------
+#           MODIFICA EL MAPA
+#-------------------------------------------------
+def Mod_Map():
+    aMapa = [
+         [ddt[str(ra.randint(0,8))] for i in range(n_MAXCOL) ] 
+          for j in range(n_MAXFIL)
+        ]
+
+
+#---------------------------------------------
+#           PARAMETROS DE FUNCIONES
+#---------------------------------------------
+sWin = Init_Pygame()
 aFig = Init_Tiles()
 
-ddt = { #Diccionario con los tiles respectivos
-       '0' : aFig[0] , '1' : aFig[1] , '2' : aFig[2], 
-       '3' : aFig[3] , '4' : aFig[4] , '5' : aFig[5], 
-       '6' : aFig[6] , '7' : aFig[7] , '8' : aFig[8], 
-       '9' : aFig[9]             
-      }#key| value
-
-matriz_Map =[
-            [ddt[str(ra.randint(0,8))] for i in range(nMAX_COL) #matriz con una lista de
-             for j in range(nMAX_FIL)]                          #listas por filas y columnas
-            ]
+ddt = { # Diccionario de Datos.. (Key|Value)
+    '0' : aFig[0] , '1' : aFig[1] , '2' : aFig[2], 
+    '3' : aFig[3] , '4' : aFig[4] , '5' : aFig[5], 
+    '6' : aFig[6] , '7' : aFig[7] , '8' : aFig[8], 
+    '9' : aFig[9]           
+    }
+aMapa = [
+         [ddt[str(ra.randint(0,8))] for i in range(n_MAXCOL) ] 
+          for j in range(n_MAXFIL)
+        ]
 
 clock = pg.time.Clock()
-while bGo:
-    click_K = pg.key.get_pressed()
-    if click_K[pg.K_ESCAPE]:                   
-        bGo = False
-    #if click_K[pg.K_F1]:                      #modifica el mapa con F1
-        #mod_Mapa()                  
-    if click_K[pg.K_F2]:                      #funcion de screenshot
-        pg.image.save(sWin,'ssMAP.png') 
+while fGO:
+    click_k = pg.key.get_pressed()
+    if click_k[pg.K_ESCAPE]:
+        fGO = False
+    if click_k[pg.K_1]:
+        Mod_Map()
+    if click_k[pg.K_2]:
+        pg.image.save(sWin, 'Screenshot Mapa.png')
 
-    ev = pg.event.get()                      #Obtiene todos los eventos de la ventana
-    for e in ev:                              
-        if e.type == QUIT:
-            bGo = False
-        if e.type == pg.MOUSEMOTION:
-            nMOUSE_x,nMOUSE_y = e.pos
-    
+    ev = pg.event.get()
+    for e in ev:
+        if e.type == QUIT: 
+            lGo = False
+        if e.type == pg.MOUSEMOTION: 
+            n_MOUSEx, n_MOUSEy = e.pos 
     Show_Map()
     Show_Mouse()
-    pg.display.flip()                        # Actualiza el contenido de la pantalla
-    clock[0].tick(100)
-    #-- completaaaaaar...
-
-pg.quit                                      #cierra pygame
+    pg.display.flip()
+    clock.tick(100)
+pg.quit
