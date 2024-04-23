f1e447d560658648f70ec816c2cbd71ff96123ab
Champitax <166671426+Champitax@users.noreply.github.com>
2024-04-23 03:05:53 -0400

    Update Proyecto_1.2.py

--- Proyecto_1.2.py
+++ Proyecto_1.2.py
@@ -1,8 +1,9 @@
+# Importamos los módulos necesarios
 from pygame.locals import *
 import pygame as pg, random as ra, time as ti, ctypes as ct  
 
-nRES = (800,500) ; lOK = True ; nMAX_GUN = 20 ; prendi_ammo = 1 ; apaga_ammo = 0; 
-nBTN_RIGHT = 3  ; nMAX_X = 800
+B_size = (800,500) ; lOK = True ; Max_ammo = 20 ; prendi_ammo = 1 ; apaga_ammo = 0; 
+Click_derecho = 3  ; nMAX_X = 800
 
 class eGun_D(ct.Structure): # Estructura Armamento 
  _fields_ = [
@@ -40,7 +41,7 @@ def Pausa():
 def PyGame_Init():
     pg.init()
     pg.display.set_caption('Nave pium pium ')
-    return pg.display.set_mode(nRES)
+    return pg.display.set_mode(B_size)
 
 #---------------------------------------------------------------------
 # Pinta la Pantalla Principal de PyGame.-
@@ -55,7 +56,8 @@ def coloca_BG():
 def Coloca_mapa():
     Bm.blit(Bk,(0, 0)) # 
     return
-
+#---------------------------------------------------------------------
+# Función para el bucle del fondo
 #---------------------------------------------------------------------
 def Bucle_BG():
     Ma = pg.Surface((2,500)) #2       #2
@@ -63,20 +65,22 @@ def Bucle_BG():
     Bm.blit(Bm,(-2,0)) #2 
     Bm.blit(Ma,(798,0)) #
     return
-
+#---------------------------------------------------------------------
+# Función para dibujar la nave en pantalla
 #---------------------------------------------------------------------
 def Dibuja_Nae(nX,nY):
     Wm.blit(Nv,(nX,nY)) #pone nave en pantalla principal
     return
-
+# Función para dibujar las municiones
 def Dibuja_ammo():
-    for i in range(nMAX_GUN):
+    for i in range(Max_ammo):
      if E_Ammo[i][1].lF: Wm.blit(M1,(E_Ammo[i][1].nX,E_Ammo[i][1].nY))
     return
-
+#---------------------------------------------------------------------
+# Función para inicializar el armamento
 #---------------------------------------------------------------------
 def Init_Gun_D():
-    for i in range(nMAX_GUN):
+    for i in range(Max_ammo):
      E_Ammo[i][0] =     prendi_ammo # 1: Disponible - 0: No Disponible
      E_Ammo[i][1].nX = +900 # Posicion X
      E_Ammo[i][1].nY = +600 # Posicion Y
@@ -85,15 +89,20 @@ def Init_Gun_D():
      E_Ammo[i][1].nD = +001 # Subiendo.-
      E_Ammo[i][1].lF =  prendi_ammo # 1: Se grafica - 0: No se grafica
     return
+#---------------------------------------------------------------------
 # Inicializa Pygame y el mixer de sonido
+#---------------------------------------------------------------------
 pg.init()
 pg.mixer.init()
-
+#---------------------------------------------------------------------
 # Carga el sonido del disparo
+#---------------------------------------------------------------------
 sonido_disparo = pg.mixer.Sound("Disparo.wav")
 #---------------------------------------------------------------------
+#Función para poner una munición 
+#---------------------------------------------------------------------
 def Pone_ammo_centro(nX,nY):
-    for i in range(nMAX_GUN):
+    for i in range(Max_ammo):
         if E_Ammo[i][0] == prendi_ammo:
             E_Ammo[i][0] = apaga_ammo
             E_Ammo[i][1].nX = nX
@@ -102,16 +111,18 @@ def Pone_ammo_centro(nX,nY):
             sonido_disparo.play()  # Reproduce el sonido del disparo
             return
         
-
+#---------------------------------------------------------------------
+# AJUSTE PARA PONER LAS BALAS EN EL CENTRO DE LA NAVE
 #---------------------------------------------------------------------
 def pone_ammo(nX,nY):
     Pone_ammo_centro(nX+70,nY+57)
     Show_Status()
     return
-
+#---------------------------------------------------------------------
+# Función para el movimiento y recarga de las municiones
 #---------------------------------------------------------------------
 def Recarga_ammo():
-    for i in range(nMAX_GUN):
+    for i in range(Max_ammo):
         if E_Ammo[i][1].lF:
             E_Ammo[i][1].nX += E_Ammo[i][1].nV * E_Ammo[i][1].nD
             if E_Ammo[i][1].nX >= nMAX_X: 
@@ -127,7 +138,7 @@ Bk = Carga_imagen('F3.png')      ; Nv = Carga_imagen('F5.png',True) ;
 M1 = Carga_imagen('F4.png',True) 
 print Bk.get_rect().size,Nv.get_rect().size,M1.get_rect().size #extra->tamano de una imagen...
 Bm = pg.Surface((800,500))     ; nMy = nMx = 3
-E_Ammo = [[3,eGun_D()] for i in range(nMAX_GUN)]
+E_Ammo = [[3,eGun_D()] for i in range(Max_ammo)]
 Init_Gun_D()
 Coloca_mapa()
 while lOK:
@@ -138,7 +149,7 @@ while lOK:
   if e.type == QUIT:
      lOK = False
   if e.type == pg.MOUSEMOTION: nMx,nMy = e.pos
-  if e.type == pg.MOUSEBUTTONDOWN and e.button == nBTN_RIGHT: Pone_ammo_centro(nMx+70,nMy+57)
+  if e.type == pg.MOUSEBUTTONDOWN and e.button == Click_derecho: Pone_ammo_centro(nMx+70,nMy+57)
 
  Bucle_BG()
  coloca_BG()
